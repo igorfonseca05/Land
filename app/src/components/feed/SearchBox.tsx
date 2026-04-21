549f322ef4b66f4135921a2f6bc4f952bc72834e
igorfonseca05 <igorfondev@gmail.com>
2026-04-21 17:11:23 -0300

    novas atualizações

--- app/src/components/feed/SearchBox.tsx
+++ app/src/components/feed/SearchBox.tsx
@@ -433,11 +433,11 @@ export function HeroSearch() {
 
             {/* Botões */}
             <div className="flex gap-3 w-full md:w-auto">
-              <Link href={`/auth/login`} className="flex-1 md:flex-none px-6 py-2 rounded-lg border border-green-500 text-green-600 text-sm font-medium hover:bg-green-50 transition">
+              <Link href={`/auth/login`} className="flex-1 text-center md:flex-none px-6 py-2 rounded-lg border border-green-500 text-green-600 text-sm font-medium hover:bg-green-50 transition">
                 Entrar
               </Link>
 
-              <Link href={'/auth/signup'} className="flex-1 md:flex-none bg-green-600 text-white px-6 py-2 rounded-lg text-sm font-medium shadow hover:bg-green-700 transition">
+              <Link href={'/auth/signup'} className="flex-1 text-center md:flex-none bg-green-600 text-white px-6 py-2 rounded-lg text-sm font-medium shadow hover:bg-green-700 transition">
                 Criar conta
               </Link>
             </div>
